03283b16a257a4b152f8f79289799056c2b7aca2
Sterben <0xsterben@tutamail.com>
2024-04-12 11:58:13 +0200

    fhkkl

--- whatyearisit-frontend/script.js
+++ whatyearisit-frontend/script.js
@@ -1,4 +1,4 @@
-fetch('https://whatyearisit-eazi-jjzaa0nv9-sterben.vercel.app/year')
+fetch('https://whatyearisit-2wdmqyspl-sterben.vercel.app/year')
     .then(response => response.json())
     .then(data => {
         document.querySelector('#year').textContent = data.year;
