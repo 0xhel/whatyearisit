bd0ff4d6d8b018570c76ba1e58a3b68535496afc
Sterben <0xsterben@tutamail.com>
2024-04-12 11:17:57 +0200

    toto

--- whatyearisit-frontend/script.js
+++ whatyearisit-frontend/script.js
@@ -1,5 +1,5 @@
-fetch('https://whatyearisit-umber.vercel.app/year')
+fetch('https://whatyearisit-q6zne4rnn-sterben.vercel.app/year')
     .then(response => response.json())
     .then(data => {
         document.querySelector('#year').textContent = data.year;
-    })
+    });
