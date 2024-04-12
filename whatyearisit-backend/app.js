825c603cb31b22eef517a4860bea56819a727726
Sterben <0xsterben@tutamail.com>
2024-04-12 11:53:08 +0200

    iuguyg

--- whatyearisit-backend/app.js
+++ whatyearisit-backend/app.js
@@ -4,8 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var yearRouter = require('./routes/year')
 
 var app = express();
 
@@ -16,7 +14,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/year', yearRouter)
 
 module.exports = app;
--- whatyearisit-backend/routes/index.js
+++ whatyearisit-backend/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/year', function (req, res, next) {
+  var currentYear = new Date().getFullYear();
+  res.json({ year: currentYear });
 });
 
 module.exports = router;
