be2d9981f19a0e3d9231f9456a49ee07b5fad7c1
Sterben <0xsterben@tutamail.com>
2024-04-12 11:26:41 +0200

    toto

--- whatyearisit-frontend/script.js
+++ whatyearisit-frontend/script.js
@@ -1,4 +1,4 @@
-fetch('https://whatyearisit-q6zne4rnn-sterben.vercel.app/year')
+fetch('whatyearisit-45m8mqna2-sterben.vercel.app/year')
     .then(response => response.json())
     .then(data => {
         document.querySelector('#year').textContent = data.year;
