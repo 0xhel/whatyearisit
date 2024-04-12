e25add5d2f05ee77cf3c9215022e09ea882937b0
Sterben <0xsterben@tutamail.com>
2024-04-12 11:04:50 +0200

    toto

--- whatyearisit-frontend/script.js
+++ whatyearisit-frontend/script.js
@@ -1,5 +1,5 @@
 fetch('https://whatyearisit-umber.vercel.app/year')
     .then(response => response.json())
     .then(data => {
-        document.querySelector('#year').textContent = year;
+        document.querySelector('#year').textContent = data.year;
     })
